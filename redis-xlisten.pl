2fba0b2baec1b67504f744def36e695eea8eaf55
Roy Ratcliffe <roy@ratcliffe.me>
2022-10-16 20:33:48 +0100

    fix: use redis_alias

--- redis-xlisten.pl
+++ redis-xlisten.pl
@@ -10,9 +10,17 @@ redis_xlisten_group_consumer(Consumer) :-
     !.
 redis_xlisten_group_consumer(Consumer) :- gethostname(Consumer).
 
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+Become a stream group consumer by listening on one or more stream keys.
+Use the host name as the consumer.
+
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
 redis_xlisten_group :-
+    setting(redis_alias, Redis),
     redis_xgroup(Group),
     redis_xlisten_group_consumer(Consumer),
     setting(redis_xgroup_create_keys, Keys),
     setting(redis_xlisten_group_options, Options),
-    xlisten_group(default, Group, Consumer, Keys, Options).
+    xlisten_group(Redis, Group, Consumer, Keys, Options).
